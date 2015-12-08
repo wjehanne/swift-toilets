18ab479aaa44b872601935370e8711d975a179e8
williamjehanne <william.jehanne@gmail.com>
2015-12-08 09:11:09 +0100

    Initial Commit

---
+++ Toilet/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Toilet
+//
+//  Created by William JEHANNE on 08/12/2015.
+//  Copyright © 2015 William JEHANNE. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
